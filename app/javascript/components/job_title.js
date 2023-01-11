5d8c689c676cec863092c8924be2700e38782836
remi <remi.tondeux@gmail.com>
2023-01-11 11:37:25 +0100

    main layout

--- app/javascript/components/job_title.js
+++ app/javascript/components/job_title.js
@@ -5,7 +5,7 @@ const DynamicJobTitle = () => {
   const jobTitle = document.querySelector('#job-title');
   if (jobTitle) {
     new Typed('#job-title', {
-      strings: ["Fullstack Developer ^1700", "Ruby on Rails - JavaScript - HTML - CSS ^2000"],
+      strings: ["Ingénieur et Développeur ^1700", "Ruby on Rails - JavaScript - HTML - CSS ^2000"],
       typeSpeed: 60,
       showCursor: false,
       loop: true
